b3ccee70e1121c622bdb226caffabe91d0e23b5e
mariehardy <mademoiselle.mph@gmail.com>
2020-05-20 09:59:31 +0200

    BERWDPT Marie - finished

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import "./App.css";
  
 class App extends Component {
   render() {
+
+    const formatName = (user) => {
+      return `${user.firstName} ${user.lastName}`;
+    }
+   
+    const user = {
+      firstName: 'Harper',
+      lastName: 'Perez',
+      avatarUrl: ''
+    };
+    
+    const element = (
+      <h2>
+        Hello, {formatName(user)}!
+      </h2>
+    );
+
     return (
       <div className="App">
       
-      <div className="navbar">
-          <logo></logo>
-          <collapsed></collapsed>
+      <div className="test">
+        {element}
       </div>
 
-      <div className="main">
+
+      <div className="navbar">
+      <img src="/images/ironhack-logo.svg" height="70px" alt="" />
+      
+      <img src="/images/menu-top.svg" height="70px" className="menu" alt="" />
+      </div>
+
+
         <div className="main-top">
         <h1>Say Hello to<br />
         ReactJS</h1>
         <h2>You will learn a Frontend<br /> framework from scratch</h2>
+        <a href="#" class="btn-awesome">Awesome</a>
         </div>
         <div className="main-bottom">
             <ul className="icon-container">
@@ -23,8 +47,22 @@ class App extends Component {
                 <li><h4>Declarative</h4></li>
                 <li><p>React makes it painless to create interactive UIs.</p></li>
             </ul>
+            <ul className="icon-container">
+                <li><img src="/images/icon2.png" alt="" /></li>
+                <li><h4>Declarative</h4></li>
+                <li><p>React makes it painless to create interactive UIs.</p></li>
+            </ul>
+            <ul className="icon-container">
+                <li><img src="/images/icon3.png" alt="" /></li>
+                <li><h4>Declarative</h4></li>
+                <li><p>React makes it painless to create interactive UIs.</p></li>
+            </ul>
+            <ul className="icon-container">
+                <li><img src="/images/icon4.png" alt="" /></li>
+                <li><h4>Declarative</h4></li>
+                <li><p>React makes it painless to create interactive UIs.</p></li>
+            </ul>
         </div>
-      </div>
 
       </div>
     );
